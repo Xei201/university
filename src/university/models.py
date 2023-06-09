aaabe7a38fb052ddd750350e8144d264c8808b38
Xei201 <xei201@yandex.ru>
2023-06-09 16:33:32 +0300

    final result model SQLALCHEMY

--- src/university/models.py
+++ src/university/models.py
@@ -1,8 +1,18 @@
-from sqlalchemy import TIMESTAMP, Column, String, BigInteger, Integer, ForeignKey
-from sqlalchemy.sql import func
-from fastapi_utils.guid_type import GUID, GUID_SERVER_DEFAULT_POSTGRESQL
+from sqlalchemy import TIMESTAMP, Column, String, BigInteger, Integer, ForeignKey, Table
+from fastapi_utils.guid_type import GUID_SERVER_DEFAULT_POSTGRESQL
+from sqlalchemy.orm import relationship
 
 from ..database import Base
+
+
+# Association table for student and program Many-to-many
+ProgramStudent = Table('program_student',
+    Column('program_student_id',
+        BigInteger,
+        primary_key=True,
+        server_default=GUID_SERVER_DEFAULT_POSTGRESQL),
+    Column('student_id', Integer, ForeignKey("student.student_id", ondelete="CASCADE")),
+    Column('program_id', Integer, ForeignKey("course_program.program_id", ondelete="CASCADE")))
 
 
 class FacultyType(Base):
@@ -10,8 +20,12 @@ class FacultyType(Base):
 
     name = Column(
         String(50),
-        primary_key=True,
-        server_default=GUID_SERVER_DEFAULT_POSTGRESQL)
+        primary_key=True)
+
+    groups = relationship(
+        "GroupStudent",
+        back_populates="faculty_type_name",
+    )
 
 
 class Faculty(Base):
@@ -23,6 +37,21 @@ class Faculty(Base):
         server_default=GUID_SERVER_DEFAULT_POSTGRESQL)
     name = Column(String(50), nullable=False)
 
+    groups = relationship(
+        "GroupStudent",
+        back_populates="faculty",
+    )
+    teachers = relationship(
+        "Teacher",
+        back_populates="faculty",
+    )
+    all_syllabus = relationship(
+        "Syllabus",
+        back_populates="faculty",
+        cascade="all, delete",
+        passive_deletes=True,
+    )
+
 
 class Course(Base):
     __tablename__ = 'course'
@@ -32,6 +61,25 @@ class Course(Base):
         primary_key=True,
         server_default=GUID_SERVER_DEFAULT_POSTGRESQL)
     name = Column(String(50), nullable=False)
+
+    all_syllabus = relationship(
+        "Syllabus",
+        back_populates="course",
+        cascade="all, delete",
+        passive_deletes=True,
+    )
+    course_programs = relationship(
+        "CourseProgram",
+        back_populates="course",
+        cascade="all, delete",
+        passive_deletes=True,
+    )
+    exams = relationship(
+        "Exam",
+        back_populates="course",
+        cascade="all, delete",
+        passive_deletes=True,
+    )
 
 
 class Semester(Base):
@@ -44,25 +92,50 @@ class Semester(Base):
     start_date = Column(TIMESTAMP, nullable=True)
     end_date = Column(TIMESTAMP, nullable=True)
 
+    all_syllabus = relationship(
+        "Syllabus",
+        back_populates="semester",
+        cascade="all, delete",
+        passive_deletes=True,
+    )
+
 
 class Building(Base):
     __tablename__ = 'building'
 
-    building_numer = Column(
+    building_number = Column(
         Integer,
         primary_key=True)
     address = Column(String(50))
+
+    audiences = relationship(
+        "Audience",
+        back_populates="building",
+        cascade="all, delete",
+        passive_deletes=True,
+    )
 
 
 class Audience(Base):
     __tablename__ = 'audience'
 
-    audience_numer = Column(
+    audience_number = Column(
         Integer,
         primary_key=True)
-    building = Column(
+    building_number = Column(
         Integer,
-        ForeignKey("building.building_number", ondelete="CASCADE"))
+        ForeignKey("building.building_numer", ondelete="CASCADE"))
+
+    building = relationship(
+        "Building",
+        back_populates="audiences",
+    )
+    schedules = relationship(
+        "Schedule",
+        back_populates="audience",
+        cascade="all, delete",
+        passive_deletes=True,
+    )
 
 
 class GroupStudent(Base):
@@ -72,12 +145,33 @@ class GroupStudent(Base):
         BigInteger,
         primary_key=True,
         server_default=GUID_SERVER_DEFAULT_POSTGRESQL)
-    faculty = Column(
+    faculty_id = Column(
         Integer,
-        ForeignKey("faculty.faculty_id", ondelete="CASCADE"))
+        ForeignKey("faculty.faculty_id", ondelete="SET NULL"),
+        nullable=True)
     faculty_type = Column(
         String(50),
-        ForeignKey("faculty_type.name", ondelete="CASCADE"))
+        ForeignKey("faculty_type.name", ondelete="SET NULL"),
+        nullable=True)
+
+    faculty = relationship(
+        "Faculty",
+        back_populates="groups",
+    )
+    faculty_type_name = relationship(
+        "FacultyType",
+        back_populates="groups",
+    )
+    students = relationship(
+        "Student",
+        back_populates="group",
+    )
+    schedules = relationship(
+        "Schedule",
+        back_populates="group",
+        cascade="all, delete",
+        passive_deletes=True,
+    )
 
 
 class Student(Base):
@@ -89,9 +183,31 @@ class Student(Base):
         server_default=GUID_SERVER_DEFAULT_POSTGRESQL)
     first_name = Column(String(50))
     last_name = Column(String(50))
-    group_student = Column(
+    group_id = Column(
         Integer,
-        ForeignKey("group_student.group_id", ondelete="SET NULL"))
+        ForeignKey("group_student.group_id", ondelete="SET NULL"),
+        nullable=True)
+
+    group = relationship(
+        "Group",
+        back_populates="students",
+    )
+    programs = relationship(
+        'CourseProgram',
+        secondary=ProgramStudent,
+        back_populates='students')
+    marks = relationship(
+        "Mart",
+        back_populates="student",
+        cascade="all, delete",
+        passive_deletes=True,
+    )
+    exam_marks = relationship(
+        "Mart_exam",
+        back_populates="student",
+        cascade="all, delete",
+        passive_deletes=True,
+    )
 
 
 class Teacher(Base):
@@ -103,9 +219,19 @@ class Teacher(Base):
         server_default=GUID_SERVER_DEFAULT_POSTGRESQL)
     first_name = Column(String(50))
     last_name = Column(String(50))
-    faculty = Column(
+    faculty_id = Column(
         Integer,
-        ForeignKey("faculty.faculty_id", ondelete="SET NULL"))
+        ForeignKey("faculty.faculty_id", ondelete="SET NULL"),
+        nullable=True)
+
+    faculty = relationship(
+        "Faculty",
+        back_populates="teachers",
+    )
+    course_programs = relationship(
+        "CourseProgram",
+        back_populates="teacher",
+    )
 
 
 class Syllabus(Base):
@@ -115,15 +241,28 @@ class Syllabus(Base):
         BigInteger,
         primary_key=True,
         server_default=GUID_SERVER_DEFAULT_POSTGRESQL)
-    faculty = Column(
+    faculty_id = Column(
         Integer,
         ForeignKey("faculty.faculty_id", ondelete="CASCADE"))
-    semester = Column(
+    semester_id = Column(
         Integer,
         ForeignKey("semester.semester_id", ondelete="CASCADE"))
-    course = Column(
+    course_id = Column(
         Integer,
         ForeignKey("course.course_id", ondelete="CASCADE"))
+
+    faculty = relationship(
+        "Faculty",
+        back_populates="all_syllabus",
+    )
+    semester = relationship(
+        "Semester",
+        back_populates="all_syllabus",
+    )
+    course = relationship(
+        "Course",
+        back_populates="all_syllabus",
+    )
 
 
 class CourseProgram(Base):
@@ -133,27 +272,38 @@ class CourseProgram(Base):
         BigInteger,
         primary_key=True,
         server_default=GUID_SERVER_DEFAULT_POSTGRESQL)
-    teacher = Column(
+    teacher_id = Column(
         Integer,
-        ForeignKey("teacher.teacher_id", ondelete="SET NULL"))
-    course = Column(
+        ForeignKey("teacher.teacher_id", ondelete="SET NULL"),
+        nullable=True)
+    course_id = Column(
         Integer,
         ForeignKey("course.course_id", ondelete="CASCADE"))
 
-
-class ProgramStudent(Base):
-    __tablename__ = 'program_student'
-
-    program_student_id = Column(
-        BigInteger,
-        primary_key=True,
-        server_default=GUID_SERVER_DEFAULT_POSTGRESQL)
-    student = Column(
-        Integer,
-        ForeignKey("student.student_id", ondelete="CASCADE"))
-    program = Column(
-        Integer,
-        ForeignKey("program.program_id", ondelete="CASCADE"))
+    teacher = relationship(
+        "Teacher",
+        back_populates="course_programs",
+    )
+    course = relationship(
+        "Course",
+        back_populates="course_programs",
+    )
+    students = relationship(
+        'Student',
+        secondary=ProgramStudent,
+        back_populates='programs')
+    schedules = relationship(
+        "CourseProgram",
+        back_populates="program",
+        cascade="all, delete",
+        passive_deletes=True,
+    )
+    exercises = relationship(
+        "Exercise",
+        back_populates="program",
+        cascade="all, delete",
+        passive_deletes=True,
+    )
 
 
 class Schedule(Base):
@@ -163,15 +313,27 @@ class Schedule(Base):
         BigInteger,
         primary_key=True,
         server_default=GUID_SERVER_DEFAULT_POSTGRESQL)
-    group_student = Column(
+    group_id = Column(
         Integer,
         ForeignKey("group_student.group_id", ondelete="CASCADE"))
-    audience = Column(
+    audience_id = Column(
         Integer,
-        ForeignKey("audience.audience_numer", ondelete="CASCADE"))
-    subject = Column(
+        ForeignKey("audience.audience_number", ondelete="CASCADE"))
+    subject_ig = Column(
         Integer,
         ForeignKey("course_program.program_id", ondelete="CASCADE"))
+    group = relationship(
+        "GroupStudent",
+        back_populates="schedules",
+    )
+    audience = relationship(
+        "Audience",
+        back_populates="schedules",
+    )
+    program = relationship(
+        "CourseProgram",
+        back_populates="schedules",
+    )
 
 
 class Exercise(Base):
@@ -187,9 +349,19 @@ class Exercise(Base):
         TIMESTAMP,
         nullable=False,
         server_default=GUID_SERVER_DEFAULT_POSTGRESQL)
-    program = Column(
+    program_id = Column(
         Integer,
-        ForeignKey("program.program_id", ondelete="CASCADE"))
+        ForeignKey("course_program.program_id", ondelete="CASCADE"))
+    program = relationship(
+        "CourseProgram",
+        back_populates="exercises",
+    )
+    marks = relationship(
+        "Mart",
+        back_populates="exercise",
+        cascade="all, delete",
+        passive_deletes=True,
+    )
 
 
 class Mark(Base):
@@ -199,26 +371,68 @@ class Mark(Base):
         BigInteger,
         primary_key=True,
         server_default=GUID_SERVER_DEFAULT_POSTGRESQL)
-    student = Column(
+    student_id = Column(
         Integer,
         ForeignKey("student.student_id", ondelete="CASCADE"))
-    exercise = Column(
+    exercise_id = Column(
         Integer,
         ForeignKey("exercise.exercise_numer", ondelete="CASCADE"))
 
+    student = relationship(
+        "Student",
+        back_populates="marks",
+    )
+    exercise = relationship(
+        "Exercise",
+        back_populates="marks",
+    )
 
-class Mark(Base):
-    __tablename__ = 'mark'
 
-    mark_id = Column(
+class Exam(Base):
+    __tablename__ = 'exam'
+
+    exam_id = Column(
         BigInteger,
         primary_key=True,
         server_default=GUID_SERVER_DEFAULT_POSTGRESQL)
-    student = Column(
+    date = Column(TIMESTAMP, nullable=True)
+    course_id = Column(
+        Integer,
+        ForeignKey("course.course_id", ondelete="CASCADE"))
+    course = relationship(
+        "Course",
+        back_populates="exams",
+    )
+    exam_marks = relationship(
+        "Mart_exam",
+        back_populates="exam",
+        cascade="all, delete",
+        passive_deletes=True,
+    )
+
+
+class Mark_exam(Base):
+    __tablename__ = 'mark_exam'
+
+    mark_exam_id = Column(
+        BigInteger,
+        primary_key=True,
+        server_default=GUID_SERVER_DEFAULT_POSTGRESQL)
+    mark_id = Column(Integer, nullable=False)
+    student_id = Column(
         Integer,
         ForeignKey("student.student_id", ondelete="CASCADE"))
-    exercise = Column(
+    exam_id = Column(
         Integer,
-        ForeignKey("exercise.exercise_numer", ondelete="CASCADE"))
+        ForeignKey("exam.exam_id", ondelete="CASCADE"))
+
+    student = relationship(
+        "Student",
+        back_populates="exam_marks",
+    )
+    exam = relationship(
+        "Exercise",
+        back_populates="exam_marks",
+    )
 
 
