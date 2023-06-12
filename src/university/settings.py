d8ee1c6f69c5df8a38ae492cac1d2f7f62e3d464
Xei201 <xei201@yandex.ru>
2023-06-12 18:00:48 +0300

    feed: example settings.ini

--- src/university/settings.py
+++ src/university/settings.py
@@ -3,14 +3,14 @@ import configparser
 config = configparser.ConfigParser()
 config.read("university/settings.ini")
 
-POSTGRES_USER = "postgres"
+POSTGRES_USER = config.get("university", "POSTGRES_USER")
 
-POSTGRES_PASSWORD = config.get("university", "post_pass")
+POSTGRES_PASSWORD = config.get("university", "POSTGRES_PASSWORD")
 
-POSTGRES_HOSTNAME = "localhost"
+POSTGRES_HOSTNAME = config.get("university", "POSTGRES_HOSTNAME")
 
-DATABASE_PORT = "5432"
+DATABASE_PORT = config.get("university", "DATABASE_PORT")
 
-POSTGRES_DB = config.get("university", "post_db")
+POSTGRES_DB = config.get("university", "POSTGRES_DB")
 
 API_VERSION = "/api/v1"
